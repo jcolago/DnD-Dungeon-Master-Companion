8c498de3e9f61ec6c28e79698953d430afe8134e
jcolago <jcolago2005@gmail.com>
2023-10-17 13:12:38 -0500

    adjusted GET routes in monsters.router.js and players.router.js

--- server/routes/monsters.router.js
+++ server/routes/monsters.router.js
@@ -7,8 +7,10 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route for /monsters
-  const queryText = `SELECT * FROM "monsters";`;
-  pool.query(queryText)
+  const queryText = `SELECT "g".id, "g".game_name, "g".dm_id, "m".name, "m".size, "m".alignment, "m".armor_class, "m".armor_class, "m".hit_points, "m".speed, "m".resistances, "m".proficiency_bonus, "m".attacks, "m".displayed FROM "games" AS "g"
+  JOIN "monsters" AS "m" ON "g".id = "m".game_id
+  WHERE "g".dm_id = $1;`;
+  pool.query(queryText, [req.user.id])
   .then(result => {
     res.send(result.rows);
   })
