c71546863aeebea2a62d7d08dd4e2c8fc2e57e2f
jcolago <jcolago2005@gmail.com>
2023-10-17 15:59:29 -0500

    Imported reducers to root reducer file

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
+import players from './players.reducer';
+import monsters from './monster.reducer';
+import games from './games.reducer';
+import items from './items.reducer';
+import conditions from './conditions.reducer'
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -11,6 +16,11 @@ import user from './user.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
+  players,
+  monsters,
+  games,
+  items,
+  conditions
 });
 
 export default rootReducer;
