0d1370672847050734e5a5d47d71b941be6a3e04
jcolago <jcolago2005@gmail.com>
2023-10-19 15:07:18 -0500

    Hooked up Delete and Display buttons in PlayerTableItem to dispatch to correct route

--- src/components/PlayerTableItems/PlayerTableItem.jsx
+++ src/components/PlayerTableItems/PlayerTableItem.jsx
@@ -10,7 +10,7 @@ export default function PlayersTableItem({ player }) {
                 <td>{player.character_name}</td>
                 <td>{player.character_level}</td>
                 <td>{player.game_name}</td>
-                <td><button>Details</button><button>Delete</button><button>Display</button></td>
+                <td><button onClick={() => console.log(player.id)}>Details</button><button onClick={() => dispatch({type: "DELETE_CHARACTER", payload: player.id})}>Delete</button><button onClick={() => dispatch({ type: 'DISPLAY_PLAYER', payload: player.id})}>Display</button></td>
             </tr>
         </>
     )
