ea2609bc093d5fd85a7b60b8de7f60bc37b51d8f
jcolago <jcolago2005@gmail.com>
2023-10-19 13:50:18 -0500

    Added function to display added items in PlayerInventoryInfo.jsx to display on dom

--- src/components/PlayerInventoryInfo/PlayerInventoryInfo.jsx
+++ src/components/PlayerInventoryInfo/PlayerInventoryInfo.jsx
@@ -11,8 +11,7 @@ export default function PlayerInventoryInfo() {
     // let [newItem, setNewItem] = useState({quantity: "" , item_id: "" })
     const [itemQuantity, setItemQuantity] = useState('0');
     const [itemId, setItemId] = useState('');
-    const [newItemArray, setNewItemArray] = useState([{quantity: '', item_id: ''}]);
-    
+
     const handleQuantityChange = (event) => {
         console.log(event)
         // setNewItem({...newItem, quantity: event.target.value})
@@ -27,31 +26,41 @@ export default function PlayerInventoryInfo() {
 
     const handleAddItem = () => {
         console.log({ quantity: itemQuantity, item_id: itemId });
-        let newItem = {quantity: itemQuantity, item_id: itemId}
-        dispatch({ type: 'ADD_TO_ARRAY', payload: newItem})
+        let newItem = { quantity: itemQuantity, item_id: itemId }
+        dispatch({ type: 'ADD_TO_ARRAY', payload: newItem })
         setItemQuantity('0');
         setItemId('')
     }
 
-    return(
+    return (
         <div>
             <div>
                 <h2>Select Inventory</h2>
             </div>
             <br />
             <label>Choose Items and quantity</label>
-            <input onChange={handleQuantityChange} type="number" placeholder="Quantity" value={itemQuantity}/>
+            <input onChange={handleQuantityChange} type="number" placeholder="Quantity" value={itemQuantity} />
             <select onChange={handleItemChange} value={itemId} name="items" id="items">
                 <option value="" disabled>Please select an item</option>
                 {inventory.map(item => {
-                    return ( <option  value={item.id} key={item.id} >{item.item_name}</option>
+                    return (<option value={item.id} key={item.id} >{item.item_name}</option>
                     )
                 })}
             </select>
             <button onClick={handleAddItem}>Add Item</button>
-            <div>
-                <p>Quantity: {backpack.quantity} </p><p>Item: </p>
-            </div>
+ 
+            {backpack.map(backpackItem => {
+                console.log(backpackItem);
+                console.log(inventory)
+                let item = inventory.find(inventoryItem => Number(inventoryItem.id) === Number(backpackItem.item_id));
+                console.log(item)
+                return (
+                    <div key={backpackItem.item_id}>
+                        <p>Quantity: {backpackItem.quantity} </p>
+                        <p>Item: {item.item_name}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+export default function Review() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const
+
+}
