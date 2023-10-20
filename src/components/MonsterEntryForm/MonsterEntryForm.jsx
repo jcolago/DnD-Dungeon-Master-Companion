3a6d58dbd0418acf7cbe0c5bc76c51a7d3c5ec73
jcolago <jcolago2005@gmail.com>
2023-10-20 16:43:52 -0500

    added form and inputs to monster entry form

--- src/components/MonsterEntryForm/MonsterEntryForm.jsx
+++ src/components/MonsterEntryForm/MonsterEntryForm.jsx
@@ -6,5 +6,21 @@ export default function MonsterEntryFrom() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    
+    const [monsterInfo, setMonsterInfo] = useState({ name: '', alignment: '' , armor_class: '', hit_points: '', speed: '', resistances: '', proficiency_bonus: '', attacks: ''});
+
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Monster Name" value={monsterInfo.name}/>
+                <input type="text" placeholder="Alignment" value={monsterInfo.alignment}/>
+                <input type="number" placeholder="Armor Class" value={monsterInfo.armor_class}/>
+                <input type="number" placeholder="Hit Points" value={monsterInfo.hit_points}/>
+                <input type="number" placeholder="Speed" value={monsterInfo.speed}/>
+                <input type="text" placeholder="Resistances" value={monsterInfo.resistances}/>
+                <input type="number" placeholder="Profiviency Bonus" value={monsterInfo.proficiency_bonus}/>
+                <input type="text" placeholder="Attacks" value={monsterInfo.attacks}/>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
