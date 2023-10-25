672ef8c4a21019ac5c8d78c61dc61b3c58796ac9
jcolago <jcolago2005@gmail.com>
2023-10-25 09:22:14 -0500

    Added MUI components to monster table and monster table items

--- src/components/MonsterTable/MonsterTable.jsx
+++ src/components/MonsterTable/MonsterTable.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import MonsterTableItem from "../MonsterTableItem/MonsterTableItem";
+import { Table, TableBody, TableHead, TableRow, TableContainer, TableCell, Paper, Typography } from "@mui/material";
 
 
 export default function MonstersTable() {
@@ -14,23 +15,27 @@ export default function MonstersTable() {
     }, [])
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Monster Name</th>
-                    <th>Size</th>
-                    <th>Hit Points</th>
-                    <th>Game Name</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {monsters.map(monster => {
-                    return (
-                        <MonsterTableItem key={monster.id} monster={monster} />
-                    )
-                })}
-            </tbody>
-        </table>
+        <Paper style={{ border: "1px solid black", margin: "10px" }}>
+            <TableContainer style={{ maxWidth: "90%", margin: "auto", padding: "10px" }}>
+                <Table style={{ border: "2px solid black" }}>
+                    <TableHead style={{ border: "2px solid black" }}>
+                        <TableRow style={{ border: "2px solid black" }}>
+                            <TableCell style={{ border: "2px solid black", textAlign: "center" }}><Typography style={{ fontWeight: "bold" }}>Monster Name</Typography></TableCell>
+                            <TableCell style={{ border: "2px solid black", textAlign: "center" }}><Typography style={{ fontWeight: "bold" }}>Size</Typography></TableCell>
+                            <TableCell style={{ border: "2px solid black", textAlign: "center" }}><Typography style={{ fontWeight: "bold" }}>Hit Points</Typography></TableCell>
+                            <TableCell style={{ border: "2px solid black", textAlign: "center" }}><Typography style={{ fontWeight: "bold" }}>Game Name</Typography></TableCell>
+                            <TableCell style={{ border: "2px solid black", textAlign: "center" }}><Typography style={{ fontWeight: "bold" }}>Actions</Typography></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {monsters.map(monster => {
+                            return (
+                                <MonsterTableItem key={monster.id} monster={monster} />
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
     )
 }
--- src/components/MonsterTableItem/MonsterTableItem.jsx
+++ src/components/MonsterTableItem/MonsterTableItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { TableRow, TableCell, Button, Typography } from "@mui/material";
 
 export default function MonsterTableItem({ monster }) {
     const dispatch = useDispatch();
@@ -8,14 +9,13 @@ export default function MonsterTableItem({ monster }) {
 
     return (
         <>
-            <tr>
-                <td>{monster.name}</td>
-                <td>{monster.size}</td>
-                <td>{monster.hit_points}</td>
-                <td>{monster.game_name}</td>
-                <td><button onClick={() => history.push(`/monsterdetails/${monster.id}`)}>Details</button> 
-                <button onClick={() => dispatch({type: "DELETE_MONSTER", payload: monster.id})}>Delete</button> </td>
-            </tr>
+            <TableRow style={{border: "2px solid black"}}>
+                <TableCell style={{borderRight: "2px solid black"}}> <Typography>{monster.name}</Typography></TableCell>
+                <TableCell style={{borderRight: "2px solid black"}}><Typography>{monster.size}</Typography></TableCell>
+                <TableCell style={{borderRight: "2px solid black"}}><Typography>{monster.hit_points}</Typography></TableCell>
+                <TableCell style={{borderRight: "2px solid black"}}><Typography>{monster.game_name}</Typography></TableCell>
+                <TableCell style={{textAlign: "center"}}><Button variant="contained" onClick={() => history.push(`/monsterdetails/${monster.id}`)}>Details</Button> <Button variant="contained" onClick={() => dispatch({type: "DELETE_MONSTER", payload: monster.id})}>Delete</Button> </TableCell>
+            </TableRow>
         </>
     )
 }
