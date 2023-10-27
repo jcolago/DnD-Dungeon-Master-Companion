d37d84b32e2ddf6b65b391d2c01f5ce6107bc77f
jcolago <jcolago2005@gmail.com>
2023-10-27 15:43:23 -0500

    Adjusted padding on game view container

--- src/components/GameView/GameView.jsx
+++ src/components/GameView/GameView.jsx
@@ -14,7 +14,7 @@ console.log(players)
     return(
         <div>
             <Typography style={{textAlign: "center", textDecoration: "underline", margin: "10px"}} variant="h4">Game View</Typography>
-        <Container style={{border: "2px double black", width: "90%", backgroundColor: "rgb(128, 150, 191, .5)", paddingRight: "50px"}}>
+        <Container style={{border: "2px double black", width: "90%", backgroundColor: "rgb(128, 150, 191, .5)", paddingRight: "50px", paddingTop: "20px", paddingBottom: "20px"}}>
             {players.map(player => {
                 return( 
                     player.displayed === true && (
