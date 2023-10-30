e2d43b39cef8e2400af7dc54bfc134e49688cced
jcolago <jcolago2005@gmail.com>
2023-10-30 18:51:39 -0500

    Added test file for games router

---
+++ server/test/server.test.js
@@ -0,0 +1,28 @@
+let app = require("../server")
+let testServer = require('supertest')
+
+describe("Test the /games GET route", () => {
+   
+    test("It should return 500 code", async () => {
+        try {
+            const gamesResponse = await testServer(app)
+            .get("/api/games/")
+            expect(gamesResponse.statusCode).toEqual(500)
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+    test("It should respond 200 from games route", async () => {
+        try {
+            let agent = testServer.agent(app)
+
+            const response = await agent
+            .post("/api/games/")
+            .send({game_name: "Joe's Game"})
+            expect(response.statusCode).toEqual(200)
+        } catch (error){
+            console.log(error)
+        }
+    })
+})
